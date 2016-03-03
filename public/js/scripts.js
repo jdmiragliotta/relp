0d95dc0e6cd640121788186f37b35e5c912c2486
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-03-03 14:23:44 -0500

    Completed navbar styling

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,15 +2,37 @@ $(document).ready(function(){
   $('select').material_select();
 
   $("#login-nav").hide();
+  $("#registration-nav").hide();
 
   $("#nav-login").click(function (e) {
-     e.preventDefault();
+    e.preventDefault();
 
-
-    if($( "#login-nav").is(":hidden" )) {
-      $("#login-nav").slideDown("slow");
+    if($( "#login-nav").is(":hidden")) {
+      if($("#registration-nav").is(":hidden")){
+        $("#login-nav").slideDown("slow");
+      }else{
+        $("#registration-nav").slideUp("slow", function(){
+           $("#login-nav").slideDown("slow");
+        });
+      }
     } else {
       $("#login-nav").slideUp("slow");
+    }
+  });
+
+  $("#nav-registration").click(function (e) {
+    e.preventDefault();
+
+    if($("#registration-nav").is(":hidden")) {
+     if($("#login-nav").is(":hidden")){
+        $("#registration-nav").slideDown("slow");
+      }else{
+       $("#login-nav").slideUp("slow", function(){
+           $("#registration-nav").slideDown("slow");
+        });
+      }
+    } else {
+      $("#registration-nav").slideUp("slow");
     }
   });
 
