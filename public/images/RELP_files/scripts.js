7b931aa30652a58a0164391fecfe219cd37c3faa
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-03-06 09:42:23 -0500

    Added some styling

---
+++ public/images/RELP_files/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  $('select').material_select();
+  $('.modal-trigger').leanModal();
+
+  $("#login-nav").hide();
+  $("#registration-nav").hide();
+
+  $("#nav-login").click(function (e) {
+    e.preventDefault();
+
+    if($( "#login-nav").is(":hidden")) {
+      if($("#registration-nav").is(":hidden")){
+        $("#login-nav").slideDown("slow");
+      }else{
+        $("#registration-nav").slideUp("slow", function(){
+           $("#login-nav").slideDown("slow");
+        });
+      }
+    } else {
+      $("#login-nav").slideUp("slow");
+    }
+  });
+
+  $("#nav-registration").click(function (e) {
+    e.preventDefault();
+
+    if($("#registration-nav").is(":hidden")) {
+     if($("#login-nav").is(":hidden")){
+        $("#registration-nav").slideDown("slow");
+      }else{
+       $("#login-nav").slideUp("slow", function(){
+           $("#registration-nav").slideDown("slow");
+        });
+      }
+    } else {
+      $("#registration-nav").slideUp("slow");
+    }
+  });
+
+
+
+
+
+
+});
+
